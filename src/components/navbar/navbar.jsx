891e9b7d467da0135a3fd4b2c6cc2400bafb6e52
Jorge Castillo <jmcastillo9915@gmail.com>
2024-09-02 21:52:07 -0500

    [Add] table, pagination, menu selection, page groups

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,33 @@
-
-export const Navbar = () => {
+import { useContext, useState } from 'react'
+import styles from './navbar.module.scss'
+import imgNavbar from '@/assets/images/Logo_barra_navegación.png'
+import { AuthContext } from '../../contexts/AuthContext'
+export const Navbar = () =>{
+  const [openMenu, setOpenMenu] = useState(false)
+  const {logout} = useContext(AuthContext)
+  const handleOpenMenu = () => {
+    setOpenMenu(!openMenu)
+  }
+  const handleLogout = () => {
+    logout()
+  }
   return (
-    <div>Navbar</div>
+    <div className={styles.container}>
+      <img src={imgNavbar} alt="LogoNavbar" />
+      <div className={
+        styles.containerUser
+      }>
+        <button onClick={handleOpenMenu}>Usuario</button>
+        <div className={
+          `${styles.containerMenu} ${
+            openMenu ? styles.active : ''
+          }`
+        }>
+          <button onClick={handleLogout}>
+            Cerrar Sesión
+            </button>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/pages/groups/groups.jsx
+++ src/pages/groups/groups.jsx
@@ -1,6 +1,138 @@
+import { useState } from "react";
+import { Table } from "../../components/table/table";
+import styles from "./groups.module.scss";
+import { FaSearch } from "react-icons/fa";
+import { Pagination } from "../../components/pagination/Pagination";
 
 export const Groups = () => {
+  const [search, setSearch] = useState("");
+  const [pagination, setPagination] = useState(null);
+  const [page, setPage] = useState(1);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const labels = [
+    {
+      name: "id",
+      label: "Identificación",
+    },
+    {
+      name: "group",
+      label: "Agrupación",
+    },
+    {
+      name: "city",
+      label: "Ciudad",
+    },
+    {
+      name: "location",
+      label: "Localidad",
+    },
+    {
+      name: "actions",
+      label: "Acciones",
+    },
+  ];
+
+  const data = [
+    {
+      id: 1,
+      group: "Grupo 1",
+      city: "Bogotá",
+      location: "Localidad 1",
+    },
+    {
+      id: 2,
+      group: "Grupo 2",
+      city: "Bogotá",
+      location: "Localidad 2",
+    },
+    {
+      id: 3,
+      group: "Grupo 3",
+      city: "Bogotá",
+      location: "Localidad 3",
+    },
+    {
+      id: 4,
+      group: "Grupo 4",
+      city: "Bogotá",
+      location: "Localidad 4",
+    },
+    {
+      id: 5,
+      group: "Grupo 5",
+      city: "Bogotá",
+      location: "Localidad 5",
+    },
+    {
+      id: 6,
+      group: "Grupo 6",
+      city: "Bogotá",
+      location: "Localidad 6",
+    },
+    {
+      id: 7,
+      group: "Grupo 7",
+      city: "Bogotá",
+      location: "Localidad 7",
+    },
+    {
+      id: 8,
+      group: "Grupo 8",
+      city: "Bogotá",
+      location: "Localidad 8",
+    },
+    {
+      id: 9,
+      group: "Grupo 9",
+      city: "Bogotá",
+      location: "Localidad 9",
+    },
+    {
+      id: 10,
+      group: "Grupo 10",
+      city: "Bogotá",
+      location: "Localidad 10",
+    },
+  ];
+  const actions = [];
+  const handleOpen = () => {};
+
   return (
-    <div>Groups</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.containerHeader}>
+        <form className={styles.containerInput}>
+          <input
+            type="text"
+            placeholder="Buscar"
+            name="search"
+            value={search}
+            onChange={handleChange}
+          />
+          <button>
+            <FaSearch />
+          </button>
+        </form>
+        <div className={styles.containerButton}>
+          <button onClick={handleOpen} className={styles.button}>
+            Configuración
+          </button>
+          <button onClick={handleOpen} className={styles.button}>
+            Informes
+          </button>
+          <button onClick={handleOpen} className={styles.button}>
+            Crear
+          </button>
+        </div>
+      </div>
+      <h2>Agrupaciones</h2>
+      <Table labels={labels} data={data} actions={actions} />
+      <div className={styles.pagination}>
+        <Pagination total={pagination?.count} page={page} setPage={setPage} />
+      </div>
+    </div>
+  );
+};
