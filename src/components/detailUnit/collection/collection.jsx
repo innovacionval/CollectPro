d2187348c6e99222139d3431f6a6a7c4e59ee871
Jorge Castillo <jmcastillo9915@gmail.com>
2024-09-09 22:05:28 -0500

    [Add] component collection

--- src/components/detailUnit/collection/collection.jsx
+++ src/components/detailUnit/collection/collection.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
-
+import styles from "./collection.module.scss";
 export const Collection = () => {
+  const labels = [
+    "Pago",
+    "Fecha",
+    "Valor",
+    "Moneda",
+    "Empleado",
+    "Aplicación",
+    "Capital",
+    "Vencido",
+    "Intereses",
+    "Mora",
+    "Honorarios",
+    "Otros",
+    "Favor",
+  ];
+  const data = [
+    {
+      payment: "RECIBO CAJA: 000000",
+      date: "10/10/2024",
+      value: "$ 0",
+      currency: "COP",
+    },
+  ]
   return (
-    <div>Collection</div>
-  )
-}
+    <div className={styles.containerCollection}>
+      <div className={styles.containerBtns}>
+        <button>Asignar responsable de cartera</button>
+        <button>Nuevo</button>
+      </div>
+      <div className={styles.containerTable}>
+        <h3>Pagos Recibidos</h3>
+        <table className={styles.tableCollection}>
+          <thead>
+            <tr>
+              {labels.map((label, index) => (
+                <th key={index}>{label}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.payment}</td>
+                <td>{item.date}</td>
+                <td>{item.value}</td>
+                <td>{item.currency}</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className={styles.containerTotal}>
+          <p>$ 0</p>
+        </div>
+      </div>
+    </div>
+  );
+};
